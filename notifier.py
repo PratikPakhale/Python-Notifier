6ffcba371a78439f671d21cec08e6141258d67e3
PratikPakhale <pratikpakhale20@gmail.com>
2021-11-16 14:08:47 +0530

    final update

--- notifier.py
+++ notifier.py
@@ -1,25 +1,33 @@
-import base64
 import requests
-import json
+from bs4 import BeautifulSoup
+import time
 from pushbullet import PushBullet
 
-user = "PratikPakhale"
-repo_name = "Python-Notifier"
-path_to_file = "config.json"
-url = 'https://api.github.com/repos/'+user + \
-    '/'+repo_name+'/contents/'+path_to_file
-
+url = "https://josaa.nic.in/WebInfo/Page/Page?PageId=1&LangId=P"
 access_token = "o.s41tTc9xuYWUBuDD2Ic2dDcISovBSPhY"
+keyword = "Round 5"
+sleep_time = 60
 
 pb = PushBullet(access_token)
+count = 1
 
-# print(url)
-req = requests.get(url)
-if req.status_code == requests.codes.ok:
-    req = req.json()
+while True:
+    r = requests.get(url)
+    soup = BeautifulSoup(r.content, 'html5lib')
+    button = soup.find('a', attrs={'class': 'btn'})
+    text = button.text
+    file = open('data.txt', 'w')
+    if keyword in text:
+        push = pb.push_note(keyword, 'Gotcha!')
+        break
+    else:
+        file.write('Current Count: '+str(count))
+    time.sleep(sleep_time)
+    count += 1
 
-    print(base64.b64decode(req['content']).decode('utf-8'))
-else:
-    print('Content was not found.')
+
+# file = open('data.txt', 'w')
+# file.write(text)
+# file.close()
 
 # push = pb.push_note('Round 6 ', 'Result out')
